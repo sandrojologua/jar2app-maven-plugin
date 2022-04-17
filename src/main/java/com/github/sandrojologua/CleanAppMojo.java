4139683bb18154682fbbac781ff0ca3b1f10ce19
sandrojologua <jologuas@gmail.com>
2022-04-17 17:59:03 +0400

    Update CleanAppMojo to look at more general target directory

--- src/main/java/com/github/sandrojologua/CleanAppMojo.java
+++ src/main/java/com/github/sandrojologua/CleanAppMojo.java
@@ -4,22 +4,25 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.FileUtils;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import static com.github.sandrojologua.Utils.DEFAULT_BUNDLE_NAME;
 
 @Mojo(name = "clean", defaultPhase = LifecyclePhase.PACKAGE)
 public final class CleanAppMojo extends AbstractMojo {
-    @Parameter private String name;
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    private MavenProject project;
 
-    private final Path targetDir = Paths.get("target");
+    @Parameter private String name;
 
     @Override
     public void execute() {
+        Path targetDir = Path.of(project.getBuild().getDirectory());
+
         String bundleName = name == null ? DEFAULT_BUNDLE_NAME : name;
         try {
             FileUtils.deleteDirectory(targetDir.resolve(bundleName + ".app").toFile());
